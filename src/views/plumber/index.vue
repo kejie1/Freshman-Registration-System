733ab04e26222a5fe5855af30a222371268b2484
chuandonghuang <1950439317@qq.com>
2021-10-15 15:52:34 +0800

    readme.md

---
+++ src/views/plumber/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-tag>标签一</el-tag>
+    <el-tag type="success">标签二</el-tag>
+    <el-tag type="info">标签三</el-tag>
+    <el-tag type="warning">标签四</el-tag>
+    <el-tag type="danger">标签五</el-tag>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  mounted() {},
+  methods: {}
+}
+</script>
+<style lang='less' scoped>
+</style>
